ac2c0a572da1e0fcb936cea38b3fcb35301f669e
Enio Fernandes <ef@cic.ac>
2018-02-06 15:52:29 +0000

    html

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # File Open JS
 
-readfile.js - Open text file from node
-ajax.html - Open text file from JS
+readfile.js - Open text file from node  
+ajax.html - Open text file from JS  
+html.js - Open html file from node
---
+++ html.js
@@ -0,0 +1,8 @@
+var http = require('http')
+var fs = require('fs')
+
+http.createServer(function (req, res) {
+    text = fs.readFileSync('ajax.html', 'utf8')
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end(text)
+}).listen(process.env.PORT)
