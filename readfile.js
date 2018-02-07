2cca3b09360a9d42f91817f2904086e9d77fb28c
Enio Fernandes <ef@cic.ac>
2018-02-07 10:34:32 +0000

    utf8

--- readfile.js
+++ readfile.js
@@ -2,6 +2,6 @@ var http = require('http')
 var fs = require('fs')
 
 http.createServer(function (req, res) {
-    text = fs.readFileSync('file.txt', 'utf8')
+    text = fs.readFileSync('file.txt')
     res.end(text)
 }).listen(process.env.PORT)
